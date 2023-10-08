5a86aac6913db9cacf25b948e8f0a795f25a40d4
AlexxNikolaev <143645546+AlexxNikolaev@users.noreply.github.com>
2023-10-08 16:57:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Stack<int> numbers = new Stack<int>();
+        numbers.Push(1);
+        numbers.Push(2);
+        numbers.Push(3);
+        numbers.Push(4);
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        Console.WriteLine("\nPopping '{0}'", numbers.Pop());
+
+        Console.WriteLine("\nPeek at next item to destack: {0}", numbers.Peek());
+
+        int[] array = new int[numbers.Count * 2];
+        numbers.CopyTo(array, numbers.Count);
+
+        Console.WriteLine("\nnumbers.Count = {0}", numbers.Count);
+
+        numbers.Clear();
+
+        Console.WriteLine("\nnumbers.Count after Clear = {0}", numbers.Count);
+    }
+}
+
